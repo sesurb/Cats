bb2e9a9fa204ffde6ad9b6a287bf09d2937bd728
Scott Surber <s.surber@msn.com>
2016-03-26 09:22:59 -0600

    The working version

--- controller.js
+++ controller.js
@@ -1,7 +1,15 @@
-﻿app.controller("myController", function ($scope, catService, $timeout) {
+﻿app.controller("myController", function ($scope, catService, $timeout, $sce) {
     $scope.catURL = "0";
     $scope.getNextCat = function () {
-        $scope.catURL = catService.getCatURL();
+        //go get the next image
+        //since we are passing html we need to see how to handle that since it could be unsafe. I decided to use $sce and I injected it above and use it like below. 
+        $scope.catURL = $sce.trustAsHtml(catService.getCatURL());
+        //Wait thirty seconds then start the process over again. This will be called recursively creating a never ending loop.
+        $timeout($scope.getNextCat, 30000);
+    };
+    $scope.startCatShow = function () {
+        //Kick off the process.
+         $scope.getNextCat();
     }
 
 
--- service.js
+++ service.js
@@ -1,25 +1,31 @@
 ﻿app.factory("catService", function ($http, $timeout) {
-
+    var catUrl = "Nothing yet"
     var service = {
-        catURL: "",
+        //This is the exposed public service that will be returned and is available to the calling
         getCatURL: function () {
+            //Storing the current cat in a temp variable to show but we will also beging the process of getting the next cat image.
             var tempCat = service.catURL;
             getCat();
+            //Return the cat url we want to show.
             return tempCat;
         }
 
 
     };
-    var i = 1;
+    
+    //This is the method to get the cat image, we could do a timeout here and load it every thirty seconds but I would rather handle the business logic in the controller.
     getCat = function() {
-       // $http.get('data.json').then(function(r) {
-            service.catURL = i.toString();
-            i++;
-            //$timeout(getCat, 5000);
-       // });
+        //The get request to pull the next image. A quick look at the documentation provides a few ways to retrieve the image. I chose to return an html image tag, we could have also got
+        //the binary or xml and just referenced the url.
+       $http.get('http://thecatapi.com/api/images/get?format=html&type=gif').then(function(r) {
+           //After looking at what was returned with the link above I could tell I needed the data potion.
+           service.catURL = r.data;
+       });
     }
-
+    
+    //When the service is loaed for the first time we need to load the image.
     service.getCatURL();
 
+    //Returns the service object. Note the {} int eh service variable above. {} denotes an object.
     return service;
 })
